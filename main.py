e3bc4094175eb8ff8b9baf5c3a14dc4d012a382d
Сергей <shtem_x@mail.ru>
2024-09-23 20:41:06 +0300

    17/09

--- main.py
+++ main.py
@@ -1,18 +1,11 @@
-# This is a sample Python script.
+# Дополнительное практическое задание по модулю:
+# "Базовые структуры данных"
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-# Press the green button in the to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-text = 'Hi,'+' Maksim_Shteynberg'
-print(text)
-print(text.count('e'))
-print(text.find('berg'))
+students = {'Johnny','Bilbo','Steve','Khedrik','Aaron'}
+grades = [[5,3,3,5,4],[2,2,2,3],[4,5,5,2],[4,4,3],[5,5,5,4,5]]
+students = sorted(students)
+print('Sorted students',students)
+grades = list(map(lambda x: sum(x)/len(x),grades))
+print('Среднее значение оценок:',grades)
+dict_students=dict(zip(students,grades))
+print('Result',dict_students)
